7a2efccb2a509d94a6439838eb94b14e987c7fb5
AHMET <112592483+AKdevDAG@users.noreply.github.com>
2024-02-04 20:47:26 +0300

    Update Soru3.c

--- Soru3.c
+++ Soru3.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Decimal'i binary'e çeviren kod
 
 void decToBinary(int n) 
 { 
-    // array to store binary number 
+
     int binaryNum[32]; 
    
-    // counter for binary array 
     int i = 0; 
     while (n > 0) { 
-        // storing remainder in binary array 
         binaryNum[i] = n % 2; 
         n = n / 2; 
         i++; 
     } 
-   
-    // printing binary array in reverse order 
     for (int j = i - 1; j >= 0; j--) 
         printf("%d", binaryNum[j]); 
 } 
+
+//enumlarımızı tanımladık
 
 enum G_range
 {
@@ -51,6 +50,8 @@ enum Operation_Mode
     DeepSuspend //101
 };
 
+//enumları struct'ın içine attık
+
 struct Config
 {
    enum G_range gr;
@@ -64,7 +65,8 @@ struct Config
 
 int main()
 {  
-    struct Config x ;
+    struct Config x ; //struct tanımladık
+    //Benim seçtiğim config ayarları
     x.gr = x_8G;    // 10
     x.bd = x_1000Hz; // 111
     x.om = LowPower1;  // 010
